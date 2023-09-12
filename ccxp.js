78b1795a5025b613fbb4da54529b1ed5a961e7c9
Filipe Gomes <filipegomes.ds@gmail.com>
2023-09-12 05:03:11 -0300

    testes

--- ccxp.js
+++ ccxp.js
@@ -1,28 +1,32 @@
-const dias = document.getElementById("dia")
-const horas = document.getElementById("hora")
-const minutos = document.getElementById("minuto")
-const segundos = document.getElementById("segundo")
+const dias = document.getElementById("dia");
+const horas = document.getElementById("hora");
+const minutos = document.getElementById("minuto");
+const segundos = document.getElementById("segundo");
 
 const lancamento = "17 oct 2023"
 
 function countdown(){
-    const datalancamento = new Date(lancamento)
-    const hoje = new Date()
-    const segundosTotais = (datalancamento - hoje)/1000
+    const datalancamento = new Date(lancamento);
+    const hoje = new Date();
+    const segundosTotais = (datalancamento - hoje)/1000;
 
-    const finalDias = math.floor(segundosTotais / 60 / 60 / 24)
-    const finalHoras = math.floor(segundosTotais / 60 / 60 ) % 24
-    const finalMinutos = math.floor(segundosTotais / 60 ) % 24
-    const finalSegundos = math.floor(segundosTotais) % 60
+    const finalDias = math.floor(segundosTotais / 60 / 60 / 24);
+    const finalHoras = math.floor(segundosTotais / 60 / 60 ) % 24;
+    const finalMinutos = math.floor(segundosTotais / 60 ) % 24;
+    const finalSegundos = math.floor(segundosTotais) % 60;
 
-    dias.innerHTML = (finalDias)
-    horas.innerHTML = formatoTempo (finalHoras)
-    minutos.innerHTML = formatoTempo (finalMinutos)
-    segundos.innerHTML = formatoTempo (finalSegundos)
+    dias.innerHTML = `${finalDias}D`
+    horas.innerHTML = formatoTempo (`${finalHoras}H`);
+    minutos.innerHTML = formatoTempo (`${finalMinutos}M`);
+    segundos.innerHTML = formatoTempo (`${finalSegundos}S`);
 }
-    function formatoTempo(tempo){
-        return tempo <=10? `${tempo}0` : tempo;
-}
+    
+
+// function formatoTempo(tempo){
+//     return tempo <=10? `${tempo}0` : tempo;
+// }
+
+
 
 countdown();
 setInterval(countdown , 1000) 
